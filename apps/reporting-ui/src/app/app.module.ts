17c7d3d42c82f2e8fe6a02191e5db15a28abe213
Alban Nocaj <alban.nocaj@gmail.com>
2022-02-22 16:25:03 +0100

    remove boilerplate component

--- apps/reporting-ui/src/app/app.module.ts
+++ apps/reporting-ui/src/app/app.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ListItemComponent } from './components/list-item/list-item.component';
 import { ListComponent } from './components/list/list.component';
 import { ReportsComponent } from './reports/reports.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, ListItemComponent, ListComponent, ReportsComponent],
+  declarations: [AppComponent, ListItemComponent, ListComponent, ReportsComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
