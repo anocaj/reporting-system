f1bac1e8af49445ca2c9d2668b8da44d57f16abd
Alban Nocaj <alban.nocaj@gmail.com>
2022-02-22 15:44:30 +0100

    remove boilerplate api endpoints

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ReportsModule } from '../reports/reports.module';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [ReportsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
